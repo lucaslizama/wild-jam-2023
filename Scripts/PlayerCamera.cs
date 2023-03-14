770cac98c0069d89f0e95d59fb6089bac2014dba
Lucas Lizama <lucaslizama4@gmail.com>
2023-03-14 03:36:25 -0300

    Adds smooth follow camera for player

---
+++ Scripts/PlayerCamera.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+public partial class PlayerCamera : Camera2D
+{
+  [Export] private TileMap tileMap;
+
+  // Called when the node enters the scene tree for the first time.
+  public override void _Ready()
+  {
+    var mapRect = tileMap.GetUsedRect();
+    var tileSize = tileMap.CellQuadrantSize;
+    var worldSizeInPixels = mapRect.Size * tileSize;
+    LimitRight = worldSizeInPixels.X;
+    LimitBottom = worldSizeInPixels.Y;
+  }
+}
