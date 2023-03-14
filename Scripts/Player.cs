4041ef125dd59f61cd199972678d3df21239c354
Lucas Lizama <lucaslizama4@gmail.com>
2023-03-14 02:48:18 -0300

    Fixed player Scene referencing Player script in wrong path

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -10,38 +10,38 @@ public partial class Player : CharacterBody2D
 
   public override void _Ready()
   {
-	gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
+    gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
   }
 
   public override void _PhysicsProcess(double delta)
   {
-	ApplyGravity(delta);
-	Jump();
-	ApplyHorizontalSpeed();
-	MoveAndSlide();
+    ApplyGravity(delta);
+    Jump();
+    ApplyHorizontalSpeed();
+    MoveAndSlide();
   }
 
   public void ApplyGravity(double delta)
   {
-	if (!IsOnFloor()) Velocity += Vector2.Down * gravity * gravityScale * (float)delta;
+    if (!IsOnFloor()) Velocity += Vector2.Down * gravity * gravityScale * (float)delta;
   }
 
   public void Jump()
   {
-	if (Input.IsActionJustPressed("jump") && IsOnFloor())
-	{
-	  Velocity += Vector2.Up * jumpVelocity;
-	}
+    if (Input.IsActionJustPressed("jump") && IsOnFloor())
+    {
+      Velocity += Vector2.Up * jumpVelocity;
+    }
   }
 
   public void ApplyHorizontalSpeed()
   {
-	var direction = Vector2.Zero;
-	var velocity = Velocity;
+    var direction = Vector2.Zero;
+    var velocity = Velocity;
 
-	direction.X += Input.IsActionPressed("right") ? 1f : Input.IsActionPressed("left") ? -1f : 0f;
-	velocity.X = direction != Vector2.Zero ? direction.X * speed : Mathf.MoveToward(velocity.X, 0, speed);
+    direction.X += Input.IsActionPressed("right") ? 1f : Input.IsActionPressed("left") ? -1f : 0f;
+    velocity.X = direction != Vector2.Zero ? direction.X * speed : Mathf.MoveToward(velocity.X, 0, speed);
 
-	Velocity = velocity;
+    Velocity = velocity;
   }
 }
