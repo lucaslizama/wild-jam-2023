68d8cc2a59ecb4ccbb1a726a7b1269918709347b
Lucas Lizama Monje <lucaslizama3@hotmail.com>
2023-03-21 01:26:18 -0300

    Refactors WoodenBox script OnArea2DBodyEntered function

--- Scripts/WoodenBox.cs
+++ Scripts/WoodenBox.cs
@@ -5,17 +5,13 @@ namespace WildJam2023.Scripts;
 
 public partial class WoodenBox : StaticBody2D
 {
-	private PackedScene destroyedVfx;
-
 	private void OnArea2DBodyEntered(Node2D body)
 	{
-		if (body is Bomb bomb)
-		{
-			var vfx = GetNode<CpuParticles2D>("VFX_WoodenBoxDestroy");
-			vfx.Emitting = true;
-			vfx.Reparent(this.GetRootNode());
+		if (body is not Bomb bomb) return;
+		var vfx = GetNode<CpuParticles2D>("VFX_WoodenBoxDestroy");
+		vfx.Emitting = true;
+		vfx.Reparent(this.GetRootNode());
 
-			QueueFree();
-		}
+		QueueFree();
 	}
 }
