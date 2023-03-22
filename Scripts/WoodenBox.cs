c0ca68823fedfe9bb71393381983914735b26406
Lucas Lizama <lucaslizama4@gmail.com>
2023-03-22 02:42:05 -0300

    WoodenBox now removes itself from the scene after VFX finish playing

--- Scripts/WoodenBox.cs
+++ Scripts/WoodenBox.cs
@@ -5,13 +5,31 @@ namespace WildJam2023.Scripts;
 
 public partial class WoodenBox : StaticBody2D
 {
-	private void OnArea2DBodyEntered(Node2D body)
-	{
-		if (body is not Bomb bomb) return;
-		var vfx = GetNode<CpuParticles2D>("VFX_WoodenBoxDestroy");
-		vfx.Emitting = true;
-		vfx.Reparent(this.GetRootNode());
+	private Sprite2D sprite;
+	private Timer destroyTimer;
+	private CpuParticles2D destroyVfx;
+	private CollisionShape2D collider;
+	private CollisionShape2D trigger;
 
-		QueueFree();
+	public override void _Ready()
+	{
+		sprite = GetNode<Sprite2D>("Sprite2D");
+		destroyTimer = GetNode<Timer>("CleanTimer");
+		destroyVfx = GetNode<CpuParticles2D>("DestroyVFX");
+		collider = GetNode<CollisionShape2D>("CollisionShape2D");
+		trigger = GetNode<CollisionShape2D>("Area2D/CollisionShape2D");
 	}
+
+	private async void OnArea2DBodyEntered(Node2D body)
+	{
+		if (body is not Bomb) return;
+		sprite.Hide();
+		destroyVfx.Emitting = true;
+		collider.SetDeferred("disabled", true);
+		trigger.SetDeferred("disabled", true);
+		await ToSignal(GetTree(), SceneTree.SignalName.ProcessFrame);
+		destroyTimer.Start();
+	}
+
+	private void OnCleanTimerTimeout() => QueueFree();
 }
